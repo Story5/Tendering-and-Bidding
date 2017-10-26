1d70aa8adc2441db339951e1957253e468dc0ec6
Story5 <337995545@qq.com>
2017-10-26 15:15:48 +0800

    [(ngModel)] bind data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,10 +5,39 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+
+  // 评标基准价系数
+  baseCoefficient : number;
+  // 满分
+  fullScore : number;
+  // 高1%扣
+  highDeduct : number;
+  // 底1%扣
+  lowDeduct : number;
+  // 去掉的家数
+  reduceUnit : number;
+  // 控制价
+  controlPrice : number;
+
+  // 报价
+  quoteArray : Array<number>;
+
+  // 基准价
+  basePrice : number;
+  // 平均价
+  averagePrice : number;
+  // 平均价 * 0.85
+  averagePrice85percent : number;
 
   constructor(public navCtrl: NavController) {
 
   }
+
+  calculate() {
+    alert(this.fullScore);
+  }
+  
 
 }
